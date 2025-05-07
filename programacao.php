481544682347789c96da2ae2d6cd3fdd03d3f35a
Arthur <guy.art123.ad@gmail.com>
2025-05-07 09:55:35 -0300

    criando site e adicionando arquivos essenciais

---
+++ programacao.php
@@ -0,0 +1,90 @@
+<?php
+require_once('html_header.php');
+require_once('header.php');
+?>
+
+<!--==========================
+            Programação Geral
+        ============================-->
+
+
+<section id="schedule" class="section-with-bg">
+    <div class="container wow fadeInUp">
+        <div class="section-header">
+            <h2>PROGRAMAÇÃO DO EVENTO</h2>
+        </div>
+
+        <!--  <h3 class="sub-heading">Organização - PETComp</h3>-->
+
+        <div class="tab-content row justify-content-center">
+
+            <!-- Schdule Day 1 -->
+            <div role="tabpanel" class="col-lg-9 tab-pane fade show active" id="day-1">
+                <h2 class="schedule-day-title">Dia XX</h2>
+                <h3 class="schedule-day-title">MANHÃ</h3>
+                
+                <div class="infos">
+                    <time>08:30-09:00</time>
+                    <img src="img/speakers/pet.jpg" alt="">
+                    <div class="sub-infos">
+                        <h4>Recepção</h4>
+                        <p>Apresentador(a): PETComp<br>Local: A definir</p>
+                    </div>
+                </div>
+
+                <div class="infos">
+                    <time>09:00-10:30</time>
+                    <img src="img/speakers/pet.jpg" alt="">
+                    <div class="sub-infos">
+                        <h4>Apresentação PETComp + Dinâmica de Apresentação + MiniTour NCA </h4>
+                        <p>Apresentador(a): PETComp<br>Local: A definir</p>
+                    </div>
+                </div>
+
+                <div class="infos">
+                    <time>10:30-12:00</time>
+                    <img src="img/speakers/simara.png" alt="">
+                    <div class="sub-infos">
+                        <h4>Palestra</h4>
+                        <p>Apresentador(a): Profa. Simara Vieira<br>Local: A definir</p>
+                    </div>
+                </div>
+
+                <h3 class="schedule-day-title">TARDE</h3>
+
+                <div class="infos">
+                    <time>14:00-15:30</time>
+                    <img src="img/speakers/pet.jpg" alt="">
+                    <div class="sub-infos">
+                        <h4>Dinâmica de jogos</h4>
+                        <p>Apresentador(a): PETComp<br>Local: A definir</p>
+                    </div>
+                </div>
+
+                <div class="infos">
+                    <time>15:30-17:00</time>
+                    <img src="img/speakers/pet.jpg" alt="">
+                    <div class="sub-infos">
+                        <h4>Minicurso de lógica</h4>
+                        <p>Apresentadores: PETComp<br>Local: A definir</p>
+                    </div>
+                </div>
+
+                <!-- <div class="infos">
+                    <time>15:30-16:30</time>
+                    <img src="img/speakers/lorde.png" alt="">
+                    <div class="sub-infos">
+                        <h4>Apresentação da Atlética</h4>
+                        <p>Apresentador(a): Mylady<br>Local: Auditório 1 do CCET</p>
+                    </div>
+                </div> -->
+
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+require_once('footer.php');
+require_once('html_footer.php');
+?>
---
+++ registros.php
@@ -0,0 +1,113 @@
+<?php
+    require_once('html_header.php');
+    require_once('header.php');
+?>
+
+    <div class="section-header">
+        <h2 class="text-temp">EM BREVE</h2>
+    </div>
+
+    <!-- <div class="swiper carousel">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide"> 
+                <img  src="./img/registros/dia1/foto-2025.1.jpg" alt="">
+            </div>
+            <div class="swiper-slide">
+                <img src="./img/registros/dia1/foto-2024.2.jpg" alt="">
+            </div>
+            <div class="swiper-slide">
+                <img src="./img/registros/dia1/foto-2024.1.jpg" alt="">
+            </div>
+            <div class="swiper-slide">
+                <div class="container-registros">
+                    <img src="./img/registros/dia1/foto-2023.2.jpeg" alt="">
+                </div>
+            </div>
+
+        </div>
+        <button type="button" class="swiper-button-next"></button>
+        <button type="button" class="swiper-button-prev"></button>
+        <div class="swiper-pagination"></div>
+    </div> -->
+
+    <!-- <div class="section-header">
+        <h2>DIA 2</h2>
+    </div>
+
+    <div class="swiper carousel">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide"> 
+                <div class="container-registros">
+                    <img class="container-registros" src="./img/registros/2025.1/dia2/foto9.jpg" alt="">
+                </div>
+            </div>
+            <div class="swiper-slide"> 
+                <div class="container-registros">
+                    <img class="container-registros" src="./img/registros/2024.2/dia2/2.jpg" alt="">
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="container-registros">
+                    <img class="container-registros" src="./img/registros/2024.1/dia2/1.JPEG" alt="">
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="container-registros">
+                    <img class="container-registros" src="./img/registros/2023.2/dia2/Foto19.jpeg" alt="">
+                </div>
+            </div>
+
+        </div>
+        <button type="button" class="swiper-button-next"></button>
+        <button type="button" class="swiper-button-prev"></button>
+        <div class="swiper-pagination"></div>
+    </div> -->
+
+    <!-- <div class="section-header">
+        <h2>DIA 3</h2>
+    </div>
+
+    <div class="swiper carousel">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide"> 
+                <div class="container-registros">
+                    <img class="container-registros" src="./img/registros/2025.1/dia3/foto1.jpg" alt="">
+                </div>
+            </div>
+            <div class="swiper-slide"> 
+                <div class="container-registros">
+                    <img class="container-registros" src="./img/registros/2025.1/dia3/foto6.jpg" alt="">
+                </div>
+            </div>
+            <div class="swiper-slide"> 
+                <div class="container-registros">
+                    <img class="container-registros" src="./img/registros/2024.2/dia3/3.jpg" alt="">
+                </div>
+            </div>
+            <div class="swiper-slide"> 
+                <div class="container-registros">
+                    <img class="container-registros" src="./img/registros/2024.1/dia3/2.jpeg" alt="">
+                </div>
+            </div>
+            <div class="swiper-slide"> 
+                <div class="container-registros">
+                    <img class="container-registros" src="./img/registros/2023.2/dia3/Foto15.jpeg" alt="">
+                </div>
+            </div>
+            <div class="swiper-slide"> 
+                <div class="container-registros">
+                    <img class="container-registros" src="./img/registros/2023.2/dia3/Foto5.jpeg" alt="">
+                </div>
+            </div>
+            
+        </div>
+        <button type="button" class="swiper-button-next"></button>
+        <button type="button" class="swiper-button-prev"></button>
+        <div class="swiper-pagination"></div> 
+    </div> -->
+
+
+<?php 
+    require_once('footer.php');
+    require_once('html_footer.php');
+?>
